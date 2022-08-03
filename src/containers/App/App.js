795e6648dbfdba7079a665eeb505c0b4a32edb31
CiprianM90 <mandricel.ciprian@yahoo.com>
2022-08-03 17:51:49 +0300

    Add base structure

---
+++ src/containers/App/App.js
@@ -0,0 +1,16 @@
+import Home from "../Home/index";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/* NavigationBar */}
+        {/* Routing for all pages */}
+        <Home />
+        {/* Footer */}
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/services/TopSellingDonutsAPI.js
@@ -0,0 +1,1 @@
+// Mock that returns 5 products: https://62ea85d2ad295463258eb216.mockapi.io/donouts
