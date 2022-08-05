625df1851fd3e8ac698457631f9853ff8b004e0a
Radu-M1 <rmbasceanu@gmail.com>
2022-08-05 14:59:00 +0300

    Implemented screen size on Footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,168 +1,175 @@
-import './style.css';
+import "./style.css";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
-import { useState } from 'react';
-import ScreenSize from '../screenSize/ScreenSize';
+import { useEffect, useState } from "react";
 
 export default function Footer() {
-
-  const[isOpen, setIsOpen] = useState(false);
-  console.log(isOpen);
+  const [isOpen, setIsOpen] = useState(false);
+  // console.log(isOpen);
   const onExpandFooterMenu = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
-  
-  
+  const [isMobile, setIsMobile] = useState(null);
+
+  const checkIsMobile = () => {
+    setIsMobile(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkIsMobile);
+  }, []);
+
   return (
     <footer>
-    <ScreenSize />
-    <div className="row">
-      <div className="column newsletter-column">
-        <h2 >NEWSLETTER</h2>
-        <div id="buttons">
-          <input
-            id="buton-text"
-            type="email"
-            name="email"
-            pattern="^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$"/>
-          <input type="button" value="Subscribe" id="subscribe" />
-        </div>
-      </div>
-
-      <div className="column">
-        <div class="footer-title" onClick={onExpandFooterMenu}>
-          <h2>USEFUL INFO</h2>
-
-          <div className="arrow-icon">
-            {
-              isOpen ? (<FaChevronUp />) : (<FaChevronDown />)
-            }
-
-
+      <div>Is Mobile: {isMobile}</div>
+      <div className="row">
+        <div className="column newsletter-column">
+          <h2>NEWSLETTER</h2>
+          <div id="buttons">
+            <input
+              id="buton-text"
+              type="email"
+              name="email"
+              pattern="^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$"
+            />
+            <input type="button" value="Subscribe" id="subscribe" />
           </div>
         </div>
 
-        {
-          isOpen && (
-            <div class="paragraphs" style={{maxHeight: "1000px"}}>
+        <div className="column">
+          <div class="footer-title" onClick={onExpandFooterMenu}>
+            <h2>USEFUL INFO</h2>
+
+            <div className="arrow-icon">
+              {isOpen ? <FaChevronUp /> : <FaChevronDown />}
+            </div>
+          </div>
+
+          {isOpen && (
+            <div class="paragraphs" style={{ maxHeight: "1000px" }}>
               <ul>
                 <li>
-                  <a href="https://ro.deloittedigital.com/" target="_blank">Privacy policy</a>
+                  <a
+                    href="https://ro.deloittedigital.com/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Privacy policy
+                  </a>
                 </li>
                 <li>
-                  <a href="https://ro.deloittedigital.com/" target="_blank"
-                    >Terms AND Conditions</a
+                  <a
+                    href="https://ro.deloittedigital.com/"
+                    target="_blank"
+                    rel="noreferrer"
                   >
+                    Terms AND Conditions
+                  </a>
                 </li>
                 <li>
-                  <a href="https://ro.deloittedigital.com/" target="_blank"
-                    >Cookie Policy</a
+                  <a
+                    href="https://ro.deloittedigital.com/"
+                    target="_blank"
+                    rel="noreferrer"
                   >
+                    Cookie Policy
+                  </a>
                 </li>
                 <li>
-                  <a href="https://ro.deloittedigital.com/" target="_blank"
-                    >FAQs</a
+                  <a
+                    href="https://ro.deloittedigital.com/"
+                    target="_blank"
+                    rel="noreferrer"
                   >
+                    FAQs
+                  </a>
                 </li>
               </ul>
             </div>
-          )
-        }
-        
-      </div>
-
-      <div className="column">
-        <div className="footer-title" onClick={onExpandFooterMenu}>
-          <h2>YOU CAN FIND US IN</h2>
-
-          <div className="arrow-icon">
-            {
-              isOpen ? (<FaChevronUp />) : (<FaChevronDown />)
-            }
-          </div>
-
+          )}
         </div>
 
-        {
-          isOpen && (
-            <div class="paragraphs" style={{maxHeight: "1000px"}}>
-          <ul>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank">London, UK</a>
-            </li>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >Bucharest, RO</a
-              >
-            </li>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >Paris, FR</a
-              >
-            </li>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >Sofia, BG</a
-              >
-            </li>
-          </ul>
-        </div>
-          )
-        }
-        
-      </div>
+        <div className="column">
+          <div className="footer-title" onClick={onExpandFooterMenu}>
+            <h2>YOU CAN FIND US IN</h2>
 
-      <div className="column">
-        <div class="footer-title" onClick={onExpandFooterMenu}>
-          <h2>ABOUT GELATO & DONUTS</h2>
-
-          <div class="arrow-icon">
-            {
-              isOpen ? (<FaChevronUp />) : (<FaChevronDown />)
-            }
-          </div>
-
-        </div>
-
-        {
-          isOpen && (
-            <div class="paragraphs" style={{maxHeight: "1000px"}}>
-          <ul>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >About us</a
-              >
-            </li>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >Store locator</a
-              >
-            </li>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >Franchise</a
-              >
-            </li>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >Careers</a
-              >
-            </li>
-            <li>
-              <a href="https://ro.deloittedigital.com/" target="_blank"
-                >Contact us</a
-              >
-            </li>
-          </ul>
+            <div className="arrow-icon">
+              {isOpen ? <FaChevronUp /> : <FaChevronDown />}
             </div>
-          )
-        }
+          </div>
 
+          {isOpen && (
+            <div class="paragraphs" style={{ maxHeight: "1000px" }}>
+              <ul>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    London, UK
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    Bucharest, RO
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    Paris, FR
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    Sofia, BG
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div className="column">
+          <div class="footer-title" onClick={onExpandFooterMenu}>
+            <h2>ABOUT GELATO & DONUTS</h2>
+
+            <div class="arrow-icon">
+              {isOpen ? <FaChevronUp /> : <FaChevronDown />}
+            </div>
+          </div>
+
+          {isOpen && (
+            <div class="paragraphs" style={{ maxHeight: "1000px" }}>
+              <ul>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    About us
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    Store locator
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    Franchise
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    Careers
+                  </a>
+                </li>
+                <li>
+                  <a href="https://ro.deloittedigital.com/" target="_blank">
+                    Contact us
+                  </a>
+                </li>
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
 
-    <div id="tm">@2021 Gelato & Donuts.</div>
-  </footer>
-  )
+      <div id="tm">@2021 Gelato & Donuts.</div>
+    </footer>
+  );
 }
- 
