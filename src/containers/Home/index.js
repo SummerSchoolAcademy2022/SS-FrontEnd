80dab6f340582129826f7297705d378d39d9947b
MetalBoy2050 <metalboy2050@gmail.com>
2022-08-09 13:15:18 +0300

    Added the subscribe functionality

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,12 +2,52 @@ import React from "react";
 import './style.css';
 
 const Home = () => {
+  let address;
+  let isAccepted = false;
+  let inputRef = React.createRef();
+  let buttonRef = React.createRef();
+
+  const check = (text) => {
+    console.log(text, address)
+    const regex = new RegExp('[a-zA-Z0-9-]+@[a-zA-Z-]+\.com');
+    if(regex.test(text)) {
+      address = text;
+      isAccepted = true;
+    }
+    else
+      isAccepted = false;
+
+    return isAccepted;
+  }
+
+  const handleSubmit = (e) => {
+    console.log("form submitted");
+    e.stopPropagation();
+    e.nativeEvent.stopImmediatePropagation();
+    e.preventDefault();
+    alert("A verification email for activating your account has been submitted to your address!");
+    console.log(inputRef.current);
+    inputRef.current.value = "";
+    buttonRef.current.disabled = true;
+  }
+
+  const handleKeyUp = (e) => {
+    if(check(e.target.value)) {
+      console.log(buttonRef.current)
+      buttonRef.current.disabled = false;
+    } else {
+      buttonRef.current.disabled = true;
+    }
+  }
+
   return (
     <div>
-      <button className="default-button">Primary</button>
-      <button className="default-button">Primary</button>
-      <button className="default-button">Primary</button>
-      <button className="default-button">Primary</button>
+      <form onSubmit={handleSubmit} >
+        <div className="newsletter-subscribe">
+          <input ref={inputRef} onKeyUp={handleKeyUp} className="input" type="text" name="newsletter"/>  
+          <button ref={buttonRef} type="submit" className="input-button" disabled>Subscribe</button>
+        </div>
+      </form>
     </div>
   )
 };
